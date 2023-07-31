eeaadd1a5f13815cdcee3902fb7898841341382d
myak <myakushek@outlook.com>
2023-07-31 22:55:10 +0300

    refactor: вернул в исходную тест компонент

--- src/components/test/MediaQueryComponentTest.tsx
+++ src/components/test/MediaQueryComponentTest.tsx
@@ -7,7 +7,7 @@ const mediaQueryComponentTest = () => {
       <h1>Device Test!</h1>
       <MediaQuery minWidth={1224}>
         <p>You are a desktop or laptop</p>
-        <MediaQuery minWidth={1824} maxWidth={2000}>
+        <MediaQuery minWidth={1824}>
           <p>You also have a huge screen</p>
         </MediaQuery>
       </MediaQuery>
